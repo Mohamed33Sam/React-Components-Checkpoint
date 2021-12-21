3d2bc32f1f5acc3a74974bbbaa4cbe07f3956f4d
Mohamed <mohamed3sam69@gmail.com>
2021-12-21 21:54:26 +0200

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Address = () => {
+  return <h1> Egypt/Cairo/15may-city </h1>;
+};
+export default Address;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ProfilePhoto = () => {
+  return <img src="/710804.jpg" alt="ProfilePhoto"></img>;
+};
+export default ProfilePhoto;
